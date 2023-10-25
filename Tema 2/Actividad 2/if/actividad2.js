84b39a4b95de2f159df3cabc6b919a516bb6110e
Noemi Pazos Vaamonde <a20noemipv@iessanclemente.net>
2023-10-25 18:00:12 +0200

    Añadida actividad 2

---
+++ Tema 2/Actividad 2/if/actividad2.js
@@ -0,0 +1,16 @@
+let edad = Number(window.prompt("Introduce tu edad"));
+if (edad < 0 || edad > 100) {
+    alert("Introduce una edad válida");
+}else if (edad >= 0 && edad <= 12) {
+    alert("Estás en el rango de niño");
+}else if (edad >= 13 && edad <= 18) {
+    alert("Estás en el rango de adolescente");
+}else if (edad >= 19 && edad <= 30){
+    alert("Estás en el rango de jóven");
+}else if (edad >= 31 && edad <= 64) {
+    alert("Estás en el rango de adulto");
+}else if (edad >= 65 && edad <= 100) {
+    alert("Estás en el rango de jubilado");
+}else {
+    alert("Introduce una edad");
+}
