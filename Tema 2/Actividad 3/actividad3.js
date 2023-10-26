bd6224be76617566d6ba9b27975341a15991f7dd
Noemi Pazos Vaamonde <a20noemipv@iessanclemente.net>
2023-10-26 11:53:48 +0200

    Añadida actividad 3

---
+++ Tema 2/Actividad 3/actividad3.js
@@ -0,0 +1,15 @@
+let num = 125;
+let division = num >> 3;
+console.log("El resultado de la operación 125/8 es: " + division);
+
+let num2 = 40;
+let multiplicacion = num2 << 2;
+console.log("El resultado de la operación 40*4 es: " + multiplicacion);
+
+let num3 = 25;
+let div = num3 >> 1; 
+console.log("El resultado de la operación 25/2 es: " + div);
+
+let num4 = 10;
+let mult = num4 << 4;
+console.log("El resultado de la operación 10*16 es: " + mult);
