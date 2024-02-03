f5b80196945c0124f2020463898f920e6b129ed3
Noemi Pazos Vaamonde <a20noemipv@iessanclemente.net>
2024-02-03 12:32:32 +0100

    Añadidas modificaciones en función mostrarInfo

--- Tema 4/Tarea 2/JavaScript/disco.js
+++ Tema 4/Tarea 2/JavaScript/disco.js
@@ -18,10 +18,10 @@ class Disco {
     }
 
     mostrarInfo() {
-        return  "<hr>" + "nombre disco: " + this.nombre + "</br>" + 
-                "grupo musical o cantante: " + this.cantante + "</br>" +
-                "año publicación: " + this.publicacion + "</br>" +
-                "tipo de música: " + this.musica + "</br>" +
-                "estado prestado: " + this.estado + "<hr>";
+        return  "<hr>" + "Nombre disco: " + this.nombre + "</br>" + 
+                "Grupo Musical o Cantante: " + this.cantante + "</br>" +
+                "Año publicación: " + this.publicacion + "</br>" +
+                "Tipo de música: " + this.musica + "</br>" +
+                "Estado prestado: " + this.estado + "<hr>";
     }
 }
